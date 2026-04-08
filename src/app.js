b24d4821d55e8b48030a1650bf88cae1368a1cf1
alejandro <alejandroulloa417@gmail.com>
2026-04-08 11:19:47 -0500

    chore: configuracion inicial del proyecto codeRide Flow

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
-export function saludar(nombre){
-return "hola " + nombre + ", bienvenido a aplicacion de codeRider!";
+export function saludar(nombre) {
+    return "hola " + nombre + ", bienvenido a aplicacion de codeRider!";
 }
 
-export function despedir (nombre){
-return "hasta pronto, " + nombre + "!codeRider";
+export function despedir(nombre) {
+    return "hasta pronto, " + nombre + "!codeRider";
 }
 
-export function estadosistema(){
-return {
-estado: "activo",
-version:"1.0.0",
-message: "sistema codeRider funcionando correctamente"
-}; 
+export function estadosistema() {
+    return {
+        estado: "activo",
+        version: "1.0.0",
+        message: "sistema codeRider funcionando correctamente"
+    };
 }
 
